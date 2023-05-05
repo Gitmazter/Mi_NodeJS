52606335ade72c6a969a2206cfc9b54d8fce9d60
gitmazter <philip.andelic@gmail.com>
2023-05-05 16:11:29 +0700

    refactored

--- Day2/lesson/commonjs/app.js
+++ Day2/lesson/commonjs/app.js
@@ -1,4 +1,5 @@
 const {getPopularMovies, getMovie} = require("./modules/movie-api");
+require('dotenv').config()
 
 //getPopularMovies().then((data) => console.log(data));
 getMovie('946311').then((data) => console.log(data))
--- Day2/lesson/commonjs/modules/HttpClient.js
+++ Day2/lesson/commonjs/modules/HttpClient.js
@@ -1,26 +1,22 @@
+const config = require("./config");
+
 class HttpClient {
+    api_key = process.env.API_KEY
+    baseUrl = process.env.BASE_URL
+
     listPopularMovies = async() => {
-        const api_key='3d76768d00521b8d87b1b0383260abfa';
-        const baseUrl='https://api.themoviedb.org/3/';
-
         const response = await fetch(
-            `${baseUrl}movie/popular?api_key=${api_key}&language=sv-SE`
+            `${this.baseUrl}movie/popular?api_key=${this.api_key}&language=sv-SE`
         );
-
-        const {results} = await response.json()
-
+        const { results } = await response.json()
         return results
     }
 
     getMovie = async(endpoint) => {
-        const api_key='3d76768d00521b8d87b1b0383260abfa';
-        const baseUrl='https://api.themoviedb.org/3/';
-
         const response = await fetch(
-            `${baseUrl}${endpoint}?api_key=${api_key}&language=sv-SE`
+            `${this.baseUrl}${endpoint}?api_key=${this.api_key}&language=sv-SE`
         );
         const movie = await response.json()
-
         return movie
     }
 }
