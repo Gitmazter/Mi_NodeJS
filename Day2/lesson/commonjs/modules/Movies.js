14ada0e9f8d98b737fb9a9cfa5e89a8800b42606
gitmazter <philip.andelic@gmail.com>
2023-05-05 14:54:38 +0700

    Day2 start

---
+++ Day2/lesson/commonjs/modules/Movies.js
@@ -0,0 +1,11 @@
+class Movie {
+    constructor(movieid, title, releaseYear, description) {
+        this.movieid = movieid;
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.description = description;
+    }
+}
+// Step 1, export the class
+// publishing an interface
+module.exports = Movie;
