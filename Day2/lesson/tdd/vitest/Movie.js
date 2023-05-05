ea449ea8ebccb8e872c2bd371a20433cd0b78ef8
gitmazter <philip.andelic@gmail.com>
2023-05-05 19:40:15 +0700

    added vitest

---
+++ Day2/lesson/tdd/vitest/Movie.js
@@ -0,0 +1,8 @@
+export class Movie {
+    constructor(id, title, releaseYear, description) {
+        this.movieId = id
+        this.title = title
+        this.releaseYear = releaseYear
+        this.description = description
+    }
+}
---
+++ Day2/lesson/tdd/vitest/movie.test.js
@@ -0,0 +1,33 @@
+import { it, describe, expect, test } from "vitest";
+import { Movie } from "./Movie";
+
+describe('Movie', () => {
+    // Arrange
+    const id = 1;
+    const title = 'The Batman'
+    const releaseYear = '1969'
+    const description = 'Itz guuud'
+    const movie = new Movie(1, title, releaseYear, description);
+
+    it('should have an id', () => {
+        expect(movie.movieId).toEqual(id);
+    })
+
+    it('should have a title', () => {
+        expect(movie.title).toEqual(title)
+    });
+
+    it('should have a release year', () => {
+        expect(movie.releaseYear).toEqual(releaseYear)
+    });
+
+    it('should have a description', () => {
+        expect(movie.description).toEqual(description)
+    });
+
+    // Act
+    it('should return an instance of Movie', () => {
+        // Assertion
+        expect(movie instanceof Movie).toBe(true);
+    });
+})
